d131f92ddaaf6ba479b59d6e60b83aa0efa4dbd0
Brht143 <i.h.elchami@gmail.com>
2024-11-14 17:32:19 +0300

    Update PetItem.js

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -3,7 +3,6 @@ import { useState } from "react";
 function PetItem({ pet }) {
 
   const [image, setImage] = useState(pet.image);
-  let count = 0;
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
@@ -11,8 +10,7 @@ function PetItem({ pet }) {
         <div className="content">
           <h3>{pet.name}</h3>
           <button type="button" className="btn btn-info" onClick={() => {
-            count += 1;
-            if (count % 2 === 0) setImage(pet.image2)
+            if (image == pet.image) setImage(pet.image2)
             else setImage(pet.image)
             }
             }>
